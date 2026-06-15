933653686ad37e44cc91bca38dfa5d94954d27cb
Collin.S <collinsobo3@gmail.com>
2026-06-15 16:03:49 -0400

    Block fix

--- components/TimeGrid.tsx
+++ components/TimeGrid.tsx
@@ -84,10 +84,10 @@ export default function TimeGrid({
                 }}
               >
                 {hasConflict && <span className={styles.warningIcon}>⚠️</span>}
+                <span className={styles.blockName}>{course.name}</span>
                 {course.classNbr && (
                   <span className={styles.blockCourseNo}>{course.classNbr}</span>
                 )}
-                <span className={styles.blockName}>{course.name}</span>
                 <span className={styles.blockTime}>
                   {course.start}–{course.end}
                 </span>
